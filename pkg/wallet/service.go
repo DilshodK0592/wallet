af6f8a3aa8819c7cb7f963114d5b1283c867771d
Dilshod K <d.kobilov@mail.ru>
2021-04-12 01:08:45 +0500

    acc add

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -93,6 +93,7 @@ func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
 	var account *types.Account
 	for _, acc := range s.accounts {
 		if acc.ID == accountID {
+			account = acc
 			return account, nil
 		}
 	}
