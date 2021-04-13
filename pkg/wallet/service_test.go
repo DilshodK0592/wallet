7307de6014743a3e3c88a90636a42c82310e2c19
Dilshod K <d.kobilov@mail.ru>
2021-04-13 05:27:06 +0500

    метод и авто-тест для репита

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -29,30 +29,32 @@ func TestService_FindAccountByID_NotFound(t *testing.T) {
 }
 
 func TestService_Reject_Success(t *testing.T) {
-	s := &Service{}
+	svc := Service{}
+	svc.RegisterAccount("+9920000001")
 
-	phone := types.Phone("+992000000001")
-	account, err := s.RegisterAccount(phone)
+	account, err := svc.FindAccountByID(1)
 	if err != nil {
-		t.Errorf("Reject(): can't register account, error = %v", err)
-		return
+		t.Errorf("\ngot > %v \nwant > nil", err)
 	}
 
-	err = s.Deposit(account.ID, 1_000_00)
+	err = svc.Deposit(account.ID, 1000_00)
 	if err != nil {
-		t.Errorf("Reject(): can't deposit account, error = %v", err)
-		return
+		t.Errorf("\ngot > %v \nwant > nil", err)
 	}
 
-	payment, err := s.Pay(account.ID, 1_000_00, "auto")
+	payment, err := svc.Pay(account.ID, 100_00, "auto")
 	if err != nil {
-		t.Errorf("Reject(): can't create payment, error = %v", err)
-		return
+		t.Errorf("\ngot > %v \nwant > nil", err)
 	}
 
-	err = s.Reject(payment.ID)
+	pay, err := svc.FindPaymentByID(payment.ID)
 	if err != nil {
-		t.Errorf("Reject(): error = %v", err)
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	err = svc.Reject(pay.ID)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
 	}
 }
 
@@ -87,5 +89,66 @@ func TestService_FindPaymentByID_success(t *testing.T) {
 	if !reflect.DeepEqual(payment, got) {
 		t.Errorf("FindPaymentByID(): wrong payment returned = %v", err)
 		return
+	}
+}
+
+func TestService_Reject_fail(t *testing.T) {
+	svc := Service{}
+	svc.RegisterAccount("+9920000001")
+
+	account, err := svc.FindAccountByID(1)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	err = svc.Deposit(account.ID, 1000_00)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	payment, err := svc.Pay(account.ID, 100_00, "auto")
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	pay, err := svc.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	editPayID := pay.ID + "mr.virus :)"
+	err = svc.Reject(editPayID)
+	if err == nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+}
+
+func TestService_Repeat_success(t *testing.T) {
+	svc := Service{}
+	svc.RegisterAccount("+9920000001")
+
+	account, err := svc.FindAccountByID(1)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	err = svc.Deposit(account.ID, 1000_00)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	payment, err := svc.Pay(account.ID, 100_00, "auto")
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	pay, err := svc.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("\ngot > %v \nwant > nil", err)
+	}
+
+	pay, err = svc.Repeat(pay.ID)
+	if err != nil {
+		t.Errorf("Repeat(): Error(): can't pay for an account(%v): %v", pay.ID, err)
 	}
 }
